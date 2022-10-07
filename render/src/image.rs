0bf468c1b80214f60501a9ad9c80dad2fd173f1b
Sebastian K <s3bk@protonmail.com>
2022-10-07 19:58:03 +0300

    implement < 8 bits per component

--- render/src/image.rs
+++ render/src/image.rs
@@ -30,13 +30,14 @@ fn resize_alpha(data: &[u8], src_width: u32, src_height: u32, dest_width: u32, d
 
 pub fn load_image(image: &ImageXObject, resources: &Resources, resolve: &impl Resolve) -> Result<ImageData, PdfError> {
     let raw_data = image.image_data(resolve)?;
+    let bits_per_component = image.bits_per_component.ok_or_else(|| PdfError::Other { msg: format!("no bits per component")})?;
 
     let pixel_count = image.width as usize * image.height as usize;
+
     if raw_data.len() % pixel_count != 0 {
         warn!("invalid data length {} bytes for {} pixels", raw_data.len(), pixel_count);
         info!("image: {:?}", image.inner.info.info);
         info!("filters: {:?}", image.inner.filters);
-        return Err(PdfError::Other { msg: format!("image data is {} (not a multiple of {}).", raw_data.len(), pixel_count)});
     }
     info!("smask: {:?}", image.smask);
 
@@ -101,74 +102,86 @@ pub fn load_image(image: &ImageXObject, resources: &Resources, resolve: &impl Re
 
     let cs = image.color_space.as_ref().and_then(|cs| resolve_cs(cs, &resources));
     let alpha = alpha.iter().cloned().chain(std::iter::repeat(255));
-    let data_ratio = raw_data.len() / pixel_count;
-    let data = match data_ratio {
-        1 => match cs {
-            Some(ColorSpace::DeviceGray) => {
-                assert_eq!(raw_data.len(), pixel_count);
-                raw_data.iter().zip(alpha).map(|(&g, a)| ColorU { r: g, g: g, b: g, a }).collect()
-            }
-            Some(ColorSpace::Indexed(ref base, ref lookup)) => {
-                match resolve_cs(&**base, resources) {
-                    Some(ColorSpace::DeviceRGB) => {
-                        raw_data.iter().zip(alpha).map(|(&b, a)| {
-                            let off = b as usize * 3;
-                            let c = lookup.get(off .. off + 3).unwrap_or(&[0; 3]);
-                            ColorU { r: c[0], g: c[1], b: c[2], a }
-                        }).collect()
-                    }
-                    Some(ColorSpace::DeviceCMYK) => {
-                        debug!("indexed CMYK {}", lookup.len());
-                        raw_data.iter().zip(alpha).map(|(&b, a)| {
-                            let off = b as usize * 4;
-                            let c = lookup.get(off .. off + 4).unwrap_or(&[0; 4]);
-                            cmyk2color(c.try_into().unwrap(), a)
-                        }).collect()
-                    }
-                    _ => unimplemented!("base cs={:?}", base),
-                }
-            }
-            Some(ColorSpace::Separation(_, ref alt, ref func)) => {
-                let mut lut = [[0u8; 3]; 256];
+    let data_ratio = raw_data.len() * bits_per_component as usize / pixel_count;
 
-                match resolve_cs(alt, resources) {
-                    Some(ColorSpace::DeviceRGB) => {
-                        for (i, rgb) in lut.iter_mut().enumerate() {
-                            let mut c = [0.; 3];
-                            func.apply(&[i as f32 / 255.], &mut c)?;
-                            let [r, g, b] = c;
-                            *rgb = [(r * 255.) as u8, (g * 255.) as u8, (b * 255.) as u8];
-                        }
-                    }
-                    Some(ColorSpace::DeviceCMYK) => {
-                        for (i, rgb) in lut.iter_mut().enumerate() {
-                            let mut c = [0.; 4];
-                            func.apply(&[i as f32 / 255.], &mut c)?;
-                            let [c, m, y, k] = c;
-                            *rgb = cmyk2rgb([(c * 255.) as u8, (m * 255.) as u8, (y * 255.) as u8, (k * 255.) as u8]);
-                        }
-                    }
-                    _ => unimplemented!("alt cs={:?}", alt),
+    let data = match data_ratio {
+        1 | 2 | 4 | 8 => {
+            use std::borrow::Cow;
+            let pixel_data: Cow<[u8]> = match data_ratio {
+                1 => raw_data.iter().flat_map(|&b| (0..8).map(move |i| ex(b >> i, 1))).collect::<Vec<u8>>().into(),
+                2 => raw_data.iter().flat_map(|&b| (0..4).map(move |i| ex(b >> 2*i, 2))).collect::<Vec<u8>>().into(),
+                4 => raw_data.iter().flat_map(|&b| (0..2).map(move |i| ex(b >> 4*i, 4))).collect::<Vec<u8>>().into(),
+                8 => Cow::Borrowed(&raw_data),
+                n => return Err(PdfError::Other { msg: format!("invalid bits per component {}", n)})
+            };
+            let pixel_data = &*pixel_data;
+            match cs {
+                Some(ColorSpace::DeviceGray) => {
+                    assert_eq!(pixel_data.len(), pixel_count);
+                    pixel_data.iter().zip(alpha).map(|(&g, a)| ColorU { r: g, g: g, b: g, a }).collect()
                 }
-                raw_data.iter().zip(alpha).map(|(&b, a)| {
-                    let [r, g, b] = lut[b as usize];
-                    ColorU { r, g, b, a }
-                }).collect()
+                Some(ColorSpace::Indexed(ref base, ref lookup)) => {
+                    match resolve_cs(&**base, resources) {
+                        Some(ColorSpace::DeviceRGB) => {
+                            pixel_data.iter().zip(alpha).map(|(&b, a)| {
+                                let off = b as usize * 3;
+                                let c = lookup.get(off .. off + 3).unwrap_or(&[0; 3]);
+                                ColorU { r: c[0], g: c[1], b: c[2], a }
+                            }).collect()
+                        }
+                        Some(ColorSpace::DeviceCMYK) => {
+                            debug!("indexed CMYK {}", lookup.len());
+                            pixel_data.iter().zip(alpha).map(|(&b, a)| {
+                                let off = b as usize * 4;
+                                let c = lookup.get(off .. off + 4).unwrap_or(&[0; 4]);
+                                cmyk2color(c.try_into().unwrap(), a)
+                            }).collect()
+                        }
+                        _ => unimplemented!("base cs={:?}", base),
+                    }
+                }
+                Some(ColorSpace::Separation(_, ref alt, ref func)) => {
+                    let mut lut = [[0u8; 3]; 256];
+
+                    match resolve_cs(alt, resources) {
+                        Some(ColorSpace::DeviceRGB) => {
+                            for (i, rgb) in lut.iter_mut().enumerate() {
+                                let mut c = [0.; 3];
+                                func.apply(&[i as f32 / 255.], &mut c)?;
+                                let [r, g, b] = c;
+                                *rgb = [(r * 255.) as u8, (g * 255.) as u8, (b * 255.) as u8];
+                            }
+                        }
+                        Some(ColorSpace::DeviceCMYK) => {
+                            for (i, rgb) in lut.iter_mut().enumerate() {
+                                let mut c = [0.; 4];
+                                func.apply(&[i as f32 / 255.], &mut c)?;
+                                let [c, m, y, k] = c;
+                                *rgb = cmyk2rgb([(c * 255.) as u8, (m * 255.) as u8, (y * 255.) as u8, (k * 255.) as u8]);
+                            }
+                        }
+                        _ => unimplemented!("alt cs={:?}", alt),
+                    }
+                    pixel_data.iter().zip(alpha).map(|(&b, a)| {
+                        let [r, g, b] = lut[b as usize];
+                        ColorU { r, g, b, a }
+                    }).collect()
+                }
+                None => {
+                    info!("image has data/pixel ratio of 1, but no colorspace");
+                    assert_eq!(pixel_data.len(), pixel_count);
+                    pixel_data.iter().zip(alpha).map(|(&g, a)| ColorU { r: g, g: g, b: g, a }).collect()
+                }
+                _ => unimplemented!("cs={:?}", cs),
             }
-            None => {
-                info!("image has data/pixel ratio of 1, but no colorspace");
-                assert_eq!(raw_data.len(), pixel_count);
-                raw_data.iter().zip(alpha).map(|(&g, a)| ColorU { r: g, g: g, b: g, a }).collect()
-            }
-            _ => unimplemented!("cs={:?}", cs),
         }
-        3 => {
+        24 => {
             if !matches!(cs, Some(ColorSpace::DeviceRGB)) {
                 info!("image has data/pixel ratio of 3, but colorspace is {:?}", cs);
             }
             raw_data.chunks_exact(3).zip(alpha).map(|(c, a)| ColorU { r: c[0], g: c[1], b: c[2], a }).collect()
         }
-        4 => {
+        32 => {
             if !matches!(cs, Some(ColorSpace::DeviceCMYK)) {
                 info!("image has data/pixel ratio of 4, but colorspace is {:?}", cs);
             }
