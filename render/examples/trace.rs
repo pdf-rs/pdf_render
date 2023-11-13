bfa36d222d6a10dd84a0b032a584dadb3a9fd1a1
Justin Bell <justin.bell@bellwether-softworks.com>
2023-11-13 14:53:56 -0600

    Fix: Trace example doesn't build

--- render/examples/trace.rs
+++ render/examples/trace.rs
@@ -1,6 +1,6 @@
-use pdf::file::{FileOptions};
-use pdf_render::tracer::{TraceCache, Tracer};
+use pdf::file::FileOptions;
 use pdf_render::render_page;
+use pdf_render::tracer::{TraceCache, Tracer};
 
 fn main() {
     env_logger::init();
@@ -8,11 +8,13 @@ fn main() {
 
     let file = FileOptions::cached().open(&arg).unwrap();
     let resolver = file.resolver();
-    
+
     let mut cache = TraceCache::new();
+
     for page in file.pages() {
         let p = page.unwrap();
-        let mut backend = Tracer::new(&mut cache);
+        let mut clip_paths = vec![];
+        let mut backend = Tracer::new(&mut cache, &mut clip_paths);
         render_page(&mut backend, &resolver, &p, Default::default()).unwrap();
         let items = backend.finish();
         for i in items {
